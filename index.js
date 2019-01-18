851d3aaba826a6e2cb9e91f1b9ef90e61c874264
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-01-18 03:40:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
-
+const main= document.querySelector('main')
+main.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1')
+newHeader.innerText = 'alyssa is a cool person'
+document.body.h1.appendChild(".victory")
+
+
